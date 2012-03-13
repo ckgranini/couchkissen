9766c6e3e51e813240d9ed90e6e071ab553c24e5
Jens Eggerstedt <ckgranini@gmail.com>
2012-03-13 14:21:49 +0100

    fix has_many dependencies

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ActiveRecord::Base
   validates :title, presence:true
   belongs_to :user
   has_many :members
-  has_many :posts, as: :postable 
+  has_many :posts, as: :postable, dependent: :delete_all
   has_many :users, :through => :members
 end
--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,5 +1,5 @@
 class Forum < ActiveRecord::Base
   validates :title, presence:true
   belongs_to :user
-  has_many :posts, as: :postable
+  has_many :posts, as: :postable, dependent: :delete_all
 end
