2078b03075520d3fe18568e04344c6df7b190378
Jens Eggerstedt <ckgranini@gmail.com>
2012-04-04 23:22:53 +0200

    movie layout, change password

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,18 @@
 module MoviesHelper
+  def get_width
+    width = params[:width]
+    if width == "1"
+      width = "w45"
+    elsif width == "2"
+      width = "w92"
+    elsif width == "3"
+      width = "w150"
+    elsif width == "4"
+      width = "w185"
+    elsif width == "original"
+      width = "original"
+    else
+      width = "w92"
+    end
+  end
 end
