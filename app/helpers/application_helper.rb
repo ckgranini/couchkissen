e508ee7b7a6132e27e6c313ee19e2696e90bde2f
CKgranini <ckgranini@gmail.com>
2013-01-06 02:02:49 +0100

    allow html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def markdown(text)
-    rndr = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
+    rndr = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: false)
     mrkd = Redcarpet::Markdown.new(rndr, autolink: true, space_after_headers: true, no_intra_emphasis: true)
     mrkd.render(text).html_safe
   end
